a733e1ff9aa0071d0c0da5ea52d9f98f61fc7e0b
Patrick Calisto <calistopatrick@gmail.com>
2024-07-08 18:14:23 -0300

    Ajuste_localstorage

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -3,13 +3,32 @@ window.addEventListener("load", function () {
   const input = document.querySelector("#new-task-input");
   const list_el = document.querySelector("#tasks");
 
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const recebe = input.value;
-    //alert.setAttribute("style", "display: inline-block");
+  // Função para salvar as tarefas no localStorage
+  function saveTasks() {
+    const tasks = [];
+    document.querySelectorAll(".tableRow").forEach(row => {
+      const task = {
+        text: row.querySelector("input.text").value,
+        star: row.querySelector("button.starFill") !== null,
+        read: row.querySelector("button.unread") !== null
+      };
+      tasks.push(task);
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+  // Função para carregar as tarefas do localStorage
+  function loadTasks() {
+    const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+    tasks.forEach(task => {
+      addTask(task.text, task.star, task.read);
+    });
+  }
+
+  // Função para adicionar uma tarefa na lista
+  function addTask(recebe, star = false, read = false) {
     if (recebe !== "") {
       const tr_el = document.createElement("div");
-
       tr_el.classList.add("tableRow");
 
       const td_content_el = document.createElement("div");
@@ -30,7 +49,8 @@ window.addEventListener("load", function () {
 
       const btn_star_el = document.createElement("button");
       btn_star_el.classList.add("star");
-      btn_star_el.innerHTML = `<img src="./img/star.svg">`;
+      btn_star_el.innerHTML = star ? `<img src="./img/star-fill.svg">` : `<img src="./img/star.svg">`;
+      if (star) btn_star_el.classList.add("starFill");
 
       const btn_edit_el = document.createElement("button");
       btn_edit_el.classList.add("edit");
@@ -42,7 +62,12 @@ window.addEventListener("load", function () {
 
       const btn_read_el = document.createElement("button");
       btn_read_el.classList.add("read");
-      btn_read_el.innerHTML = `<img src="./img/clipboard2.svg">`;
+      btn_read_el.innerHTML = read ? `<img src="./img/clipboard2-check.svg">` : `<img src="./img/clipboard2.svg">`;
+      if (read) {
+        btn_read_el.classList.add("unread");
+        table_input_el.classList.add("corMarca");
+        table_input_el.setAttribute("style", "text-decoration:line-through");
+      }
 
       btn_actions_el.appendChild(btn_star_el);
       btn_actions_el.appendChild(btn_read_el);
@@ -54,64 +79,65 @@ window.addEventListener("load", function () {
       input.value = "";
 
       btn_edit_el.addEventListener("click", (e) => {
-        if (
-          btn_edit_el.innerHTML.toLowerCase() ==
-          `<img src="./img/pencil-square.svg">`
-        ) {
+        if (btn_edit_el.classList.contains("edit")) {
           btn_edit_el.classList = "save";
           btn_edit_el.innerHTML = `<img src="./img/arrow-repeat.svg">`;
           table_input_el.setAttribute("style", "color: #f00");
           table_input_el.removeAttribute("readonly");
           table_input_el.focus();
         } else {
+          btn_edit_el.classList.remove("save");
+          btn_edit_el.classList.add("edit");
           btn_edit_el.innerHTML = `<img src="./img/pencil-square.svg">`;
           table_input_el.setAttribute("readonly", "readonly");
           table_input_el.removeAttribute("style", "color: #f00");
-          btn_edit_el.classList.remove("save");
-          btn_edit_el.classList.add("edit");
-          ativarSave(msgSave);
+          saveTasks();
         }
       });
 
       btn_delete_el.addEventListener("click", (e) => {
         list_el.removeChild(tr_el);
-        ativarD(msgD);
+        saveTasks();
       });
+
       btn_read_el.addEventListener("click", (e) => {
-        if (
-          btn_read_el.innerHTML.toLowerCase() ==
-          `<img src="./img/clipboard2.svg">`
-        ) {
+        if (btn_read_el.classList.contains("read")) {
           table_input_el.classList.add("corMarca");
           btn_read_el.classList = "unread";
           btn_read_el.innerHTML = `<img src="./img/clipboard2-check.svg">`;
-
           table_input_el.setAttribute("style", "text-decoration:line-through");
         } else {
-          btn_read_el.innerHTML = `<img src="./img/clipboard2.svg">`;
           table_input_el.classList.remove("corMarca");
           btn_read_el.classList.remove("unread");
           btn_read_el.classList.add("read");
-          table_input_el.removeAttribute(
-            "style",
-            "text-decoration:line-through"
-          );
+          btn_read_el.innerHTML = `<img src="./img/clipboard2.svg">`;
+          table_input_el.removeAttribute("style", "text-decoration:line-through");
         }
+        saveTasks();
       });
+
       btn_star_el.addEventListener("click", (e) => {
-        if (
-          btn_star_el.innerHTML.toLowerCase() == `<img src="./img/star.svg">`
-        ) {
+        if (btn_star_el.classList.contains("star")) {
           btn_star_el.classList = "starFill";
           btn_star_el.innerHTML = `<img src="./img/star-fill.svg">`;
         } else {
-          btn_star_el.innerHTML = `<img src="./img/star.svg">`;
           btn_star_el.classList.remove("starFill");
           btn_star_el.classList.add("star");
+          btn_star_el.innerHTML = `<img src="./img/star.svg">`;
         }
+        saveTasks();
       });
-    } else {
-      ativarNull(msgNull);
+
+      saveTasks();
     }
+  }
+
+  // Carregar as tarefas ao carregar a página
+  loadTasks();
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const recebe = input.value;
+    addTask(recebe);
   });
 });
